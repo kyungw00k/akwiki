8b0eb820b57c10a6d22d4a0665f2d9f668479017
Kyungwook Park <parksama@gmail.com>
2026-04-06 13:13:50 +0900

    feat: add serve command for previewing built site

--- cmd/serve.go
+++ cmd/serve.go
@@ -2,18 +2,32 @@ package cmd
 
 import (
 	"fmt"
+	"net/http"
+	"path/filepath"
+
+	"github.com/kyungw00k/akwiki/internal/config"
 	"github.com/spf13/cobra"
 )
+
+var servePort string
 
 var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Serve the built site",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("Serving dist/...")
-		return nil
+		rootDir := "."
+		cfg, err := config.Load(rootDir)
+		if err != nil {
+			return err
+		}
+		outDir := filepath.Join(rootDir, cfg.Build.OutDir)
+		addr := ":" + servePort
+		fmt.Printf("Serving %s at http://localhost%s\n", outDir, addr)
+		return http.ListenAndServe(addr, http.FileServer(http.Dir(outDir)))
 	},
 }
 
 func init() {
+	serveCmd.Flags().StringVarP(&servePort, "port", "p", "3000", "port to serve on")
 	rootCmd.AddCommand(serveCmd)
 }
